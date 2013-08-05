b7bc7b781eba6c4b5e093eaaaf30cc8314bc8490
Ulrik Günther <guenther@mpi-cbg.de>
2013-08-05 14:53:33 +0200

    Added JythonLauncher script, c.f. http://fiji.sc/Jython_Scripting#Distributing_jython_scripts_in_a_.jar_file

---
+++ JythonLauncher.java
@@ -0,0 +1,9 @@
+package my;
+import ij.plugin.PlugIn;
+import Jython.Refresh_Jython_Scripts;
+ 
+public class JythonLauncher implements PlugIn {
+    public void run(String arg) {
+        new Refresh_Jython_Scripts().runScript(getClass().getResourceAsStream(arg));
+    }
+}
